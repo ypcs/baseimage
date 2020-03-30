427241325f5418fda82c7bc939d09f7d9b2bfac8
Ville Korhonen <ville@xd.fi>
2020-03-30 17:02:06 +0300

    WIP build

--- Makefile
+++ Makefile
@@ -18,44 +18,21 @@ noop:
 
 all: clean $(DEBIAN_SUITES) $(UBUNTU_SUITES)
 
-push:
-	$(DOCKER) push $(NAMESPACE)/debian
-	$(DOCKER) push $(NAMESPACE)/ubuntu
-
 clean:
 	rm -rf *.tar *.tar.gz chroot-*
+	rm -rf tmp.*
 
-$(DEBIAN_SUITES): % : debian-%.tar import-debian-%
+$(DEBIAN_SUITES): % : debian-%.tar
 
-$(UBUNTU_SUITES): % : ubuntu-%.tar import-ubuntu-%
+$(UBUNTU_SUITES): % : ubuntu-%.tar
 
 %.tar: chroot-%
-	$(TAR) -C $< -c . -f $@
-	./scripts/lxc-metadata.sh $(patsubst %.tar,%,$@)
-
-import-debian-%: debian-%.tar
-	$(DOCKER) import - "$(NAMESPACE)/debian:$*" < $<
-
-import-ubuntu-%: ubuntu-%.tar
-	$(DOCKER) import - "$(NAMESPACE)/ubuntu:$*" < $<
-
-push-debian-%: import-debian-%
-	$(DOCKER) push "$(NAMESPACE)/debian:$*"
-
-push-ubuntu-%: import-ubuntu-%
-	$(DOCKER) push "$(NAMESPACE)/ubuntu:$*"
 
 chroot-debian-%:
-	$(DEBOOTSTRAP) $(DEBOOTSTRAP_FLAGS) $* $@ $(DEBIAN_MIRROR)
-	rsync --chown=root:root -avh rootfs/* $@/
-	chroot $@ bash -c 'DEBIAN_MIRROR="$(DEBIAN_MIRROR)" DISTRIBUTION="debian" CODENAME="$*" /bin/bash /usr/lib/baseimage-helpers/build/execute'
-	chroot $@ find / -type f sha256sum "{}" \; |tee $@/sha256sums
+	sh build.sh debian $*
 
 chroot-ubuntu-%:
-	$(DEBOOTSTRAP) $(DEBOOTSTRAP_FLAGS) $* $@ $(UBUNTU_MIRROR)
-	rsync --chown=root:root -avh rootfs/* $@/
-	chroot $@ bash -c 'UBUNTU_MIRROR="$(UBUNTU_MIRROR)" DISTRIBUTION="ubuntu" CODENAME="$*" /bin/bash /usr/lib/baseimage-helpers/build/execute'
-	chroot $@ find / -type f sha256sum "{}" \; |tee $@/sha256sums
+	sh build.sh ubuntu $*
 
 images:
 	$(MAKE) -C $@
--- build.sh
+++ build.sh
@@ -2,19 +2,32 @@
 set -e
 
 DISTRO="$1"
-RELEASE="$2"
-CACHE_DIR="$(realpath "$(dirname "$0")/cache")"
+CODENAME="$2"
+BUILD_ID="${BUILD_ID:-$(git describe --always)-$(date +%Y%m%d%H%M)}"
+
+usage() {
+    echo "usage: $0 <distribution> <codename>"
+}
+
+[ -z "${DISTRO}" ] && usage && exit 1
+[ -z "${CODENAME}" ] && usage && exit 1
+
+BASEDIR="$(realpath "$(dirname "$0")")"
+CACHE_DIR="${BASEDIR}/cache"
 
 if [ ! -d "${CACHE_DIR}" ]
 then
     mkdir -p "${CACHE_DIR}"
 fi
 
+# TODO: If specific script not foumd, fallback to latest for same distro
+# TODO: Add support for cleaning up cruft and/or use temporary build dirs
+
 case "${DISTRO}"
 in
     debian)
         MIRROR="${DEBIAN_MIRROR:-https://deb.debian.org/debian}"
-	case "${RELEASE}"
+	case "${CODENAME}"
 	in
             sid)
 	    ;;
@@ -23,68 +36,102 @@ in
             testing|stable|oldstable|oldoldstable)
 	    ;;
             *)
-                echo "Unknown/unsupported release: '${RELEASE}'."
+                echo "Unknown/unsupported release: '${CODENAME}'."
 		exit 1
 	    ;;
 	esac
     ;;
     ubuntu)
-	MIRROR="${UBUNTU_MIRROR:-https://archive.ubuntu.com/ubuntu}"
-	case "${RELEASE}"
+	MIRROR="${UBUNTU_MIRROR:-http://archive.ubuntu.com/ubuntu}"
+	case "${CODENAME}"
 	in
             eoan|bionic|xenial)
 	    ;;
             *)
-                echo "Unknown/unsupported release: '${RELEASE}'."
+                echo "Unknown/unsupported release: '${CODENAME}'."
                 exit 1
 	    ;;
 	esac
     ;;
     *)
-        echo "Invalid release '${RELEASE}'!"
+        echo "Invalid release '${CODENAME}'!"
         exit 1
     ;;
 esac
 
-if [ -z "${RELEASE}" ]
+if [ -z "${CODENAME}" ]
 then
     echo "Missing release!"
     exit 1
 fi
 
-echo "I: Building ${DISTRO}/${RELEASE} using '${MIRROR}' as mirror."
+echo "I: Building ${DISTRO}/${CODENAME} using '${MIRROR}' as mirror."
 
-TARGET="chroot-${RELEASE}"
+TEMPDIR="$(mktemp --directory "${BASEDIR}/tmp.build.${BUILD_ID}.XXXXXX")"
 
-if [ "$(id -u)" = "0" ]
+echo "I: Using '${TEMPDIR}' as temporary directory."
+
+ARTIFACTS="${BASEDIR}/artifacts/${BUILD_ID}"
+mkdir -p "${ARTIFACTS}"
+
+if [ "$(id -u)" != "0" ]
 then
-    DEBOOTSTRAP="/usr/sbin/debootstrap"
-else
-    DEBOOTSTRAP="/usr/bin/sudo /usr/sbin/debootstrap"
+    echo "E: This script must be run as root!"
+    exit 1
 fi
 
-${DEBOOTSTRAP} \
+TARGET="${TEMPDIR}/chroot-${DISTRO}-${CODENAME}"
+
+TARBALL="${ARTIFACTS}/debootstrap_${DISTRO}-${CODENAME}.tar"
+
+echo "I: Create debootstrap tarball..."
+/usr/sbin/debootstrap \
+    --make-tarball="${TARBALL}" \
     --variant=minbase \
     --force-check-gpg \
+    --log-extra-deps \
     --cache-dir="${CACHE_DIR}" \
-    "${RELEASE}" \
+    "${CODENAME}" \
+    "${TARGET}.tarball" \
+    "${MIRROR}" \
+    "/usr/share/debootstrap/scripts/${CODENAME}"
+
+echo "I: Create actual chroot directory..."
+/usr/sbin/debootstrap \
+    --unpack-tarball="${TARBALL}" \
+    --variant=minbase \
+    --force-check-gpg \
+    --log-extra-deps \
+    --cache-dir="${CACHE_DIR}" \
+    "${CODENAME}" \
     "${TARGET}" \
-    "${MIRROR}" #\
-#    "/usr/share/debootstrap/scripts/${RELEASE}"
-exit 1
-rsync --chown=root:root -avh rootfs/* "${TARGET}/"
+    "${MIRROR}" \
+    "/usr/share/debootstrap/scripts/${CODENAME}"
 
-case "${DISTRO}"
-in
-    debian)
-        chroot "${TARGET}" bash -c 'DEBIAN_MIRROR="${MIRROR}" DISTRIBUTION="debian" CODENAME="${RELEASE}" /bin/bash /usr/lib/baseimage-helpers/build/execute'
-    ;;
-    ubuntu)
-        chroot "${TARGET}" bash -c 'UBUNTU_MIRROR="${MIRROR}" DISTRIBUTION="ubuntu" CODENAME="${RELEASE}" /bin/bash /usr/lib/baseimage-helpers/build/execute'
-    ;;
-    *)
-        echo "Invalid release '${RELEASE}'!"
-        exit 1
-    ;;
-esac
+cat > "${TARGET}/envfile" << EOF
+# Environment configuration for baseimage
+export MIRROR="${MIRROR}"
+export DISTRIBUTION="${DISTRO}"
+export CODENAME="${CODENAME}"
+EOF
 
+echo "I: Sync rootfs to chroot..."
+rsync --exclude=.gitignore --chown=root:root -avh rootfs/* "${TARGET}/"
+
+tar -C "${TARGET}" -cf "${ARTIFACTS}/chroot_${DISTRO}-${CODENAME}.tar" "${TARGET}"
+
+VARIANTS_DIR="${BASEDIR}/variants"
+for variant in $(find "${VARIANTS_DIR}" -maxdepth 1 -type d -print)
+do
+    if [ "${variant}" = "${VARIANTS_DIR}" ]
+    then
+        continue
+    fi
+    VAR="$(basename "${variant}")"
+    echo "I: Build variant '${VAR}'..."
+    VTARGET="${TARGET}-${VAR}"
+    rsync -avh "${TARGET}/" "${VTARGET}"
+    rsync --exclude=.gitignore -avh "${variant}/" "${VTARGET}"
+    chroot "${VTARGET}" /bin/bash /usr/lib/baseimage-helpers/build/execute
+    tar -C "${VTARGET}" -cf "${ARTIFACTS}/variant_${DISTRO}-${CODENAME}_${VAR}.tar" "${VTARGET}"
+done
