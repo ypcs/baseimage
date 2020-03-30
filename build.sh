8ca89d7836380573d4277bcfd1c88d61423c9528
Ville Korhonen <ville@xd.fi>
2020-03-30 17:00:00 +0300

    Add some release checks

--- build.sh
+++ build.sh
@@ -3,14 +3,42 @@ set -e
 
 DISTRO="$1"
 RELEASE="$2"
+CACHE_DIR="$(realpath "$(dirname "$0")/cache")"
+
+if [ ! -d "${CACHE_DIR}" ]
+then
+    mkdir -p "${CACHE_DIR}"
+fi
 
 case "${DISTRO}"
 in
     debian)
-        MIRROR="https://deb.debian.org/debian"
+        MIRROR="${DEBIAN_MIRROR:-https://deb.debian.org/debian}"
+	case "${RELEASE}"
+	in
+            sid)
+	    ;;
+            bullseye|buster|stretch|jessie)
+            ;;
+            testing|stable|oldstable|oldoldstable)
+	    ;;
+            *)
+                echo "Unknown/unsupported release: '${RELEASE}'."
+		exit 1
+	    ;;
+	esac
     ;;
     ubuntu)
-	MIRROR="https://archive.ubuntu.com/ubuntu"
+	MIRROR="${UBUNTU_MIRROR:-https://archive.ubuntu.com/ubuntu}"
+	case "${RELEASE}"
+	in
+            eoan|bionic|xenial)
+	    ;;
+            *)
+                echo "Unknown/unsupported release: '${RELEASE}'."
+                exit 1
+	    ;;
+	esac
     ;;
     *)
         echo "Invalid release '${RELEASE}'!"
@@ -18,18 +46,32 @@ in
     ;;
 esac
 
+if [ -z "${RELEASE}" ]
+then
+    echo "Missing release!"
+    exit 1
+fi
+
 echo "I: Building ${DISTRO}/${RELEASE} using '${MIRROR}' as mirror."
 
 TARGET="chroot-${RELEASE}"
 
-/usr/sbin/debootstrap \
+if [ "$(id -u)" = "0" ]
+then
+    DEBOOTSTRAP="/usr/sbin/debootstrap"
+else
+    DEBOOTSTRAP="/usr/bin/sudo /usr/sbin/debootstrap"
+fi
+
+${DEBOOTSTRAP} \
     --variant=minbase \
     --force-check-gpg \
-    "${RELEASE}" \
+    --cache-dir="${CACHE_DIR}" \
+    "${RELEASE}" \
     "${TARGET}" \
-    "${MIRROR}" \
-    "/usr/share/debootstrap/scripts/${RELEASE}"
-
+    "${MIRROR}" #\
+#    "/usr/share/debootstrap/scripts/${RELEASE}"
+exit 1
 rsync --chown=root:root -avh rootfs/* "${TARGET}/"
 
 case "${DISTRO}"
