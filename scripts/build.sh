647973be4f3f3647ee2b680755f5c388ee9a486e
Ville Korhonen <ville@xd.fi>
2021-06-27 13:40:10 +0300

    WIP

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,7 +5,7 @@ set -x
 BASEDIR="$(dirname "$(dirname "$0")")"
 SUITE="${1:-bullseye}"
 TARGET="${BASEDIR}/build"
-TEMPDIR="$(mktemp --directory --tempdir baseimage.XXXXXX)"
+TEMPDIR="$(mktemp --directory --tmpdir baseimage.XXXXXX)"
 
 echo "Building images for '${SUITE}'."
 echo "Using '${TARGET}' as target directory."
@@ -19,14 +19,25 @@ HOOKSDIR="${TEMPDIR}/hooks"
 mkdir -p "${HOOKSDIR}"
 
 echo "I: Copy generic hooks..."
-cp -a "${BASEDIR}/hooks/"* "${TARGET}/hooks/"
+cp -a "${BASEDIR}/hooks/"* "${HOOKSDIR}/"
 
-if [ -d "${BASEDIR}/suites/${SUITE}/hooks" ]
+SUITEHOOKDIR="${BASEDIR}/suites/${SUITE}/hooks"
+if [ -d "${SUITEHOOKDIR}" ]
 then
-    cp -a "${BASEDIR}/suites/${SUITE}/hooks/"* "${TARGET}/hooks/"
+    cp -a "${SUITEHOOKDIR}/"* "${HOOKSDIR}/"
 else
     echo "W: No suite hooks directory."
 fi
+
+# Configure APT proxying if one of APT_PROXY, http_proxy or HTTP_PROXY variable
+# is set. This is the order of preference.
+APT_PROXY="${APT_PROXY:-${http_proxy:-${HTTP_PROXY}}}"
+
+if [ -n "${APT_PROXY}" ]
+then
+    echo "APT: '${APT_PROXY}'"
+fi
+exit 0
 
 #
 # Setup rootfs
